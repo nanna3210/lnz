74041dd9dedb729cfc9fb37b539b0ade37223386
nanna3210 <navkishoredas@gmail.com>
2019-12-02 18:58:22 +0530

    first commit uploading all the files

---
+++ justEx/src/justEx/Add.java
@@ -0,0 +1,18 @@
+package justEx;
+
+public class Add {
+
+	public int addNow(int no1, int no2) {
+		int r = no1 + no2;
+
+		return r;
+
+	}
+
+	public int sub(int num1, int num2) {
+
+		int r = num1 - num2;
+		return r;
+	}
+
+}
---
+++ justEx/src/justEx/SimpleAdd.java
@@ -0,0 +1,13 @@
+package justEx;
+
+public class SimpleAdd {
+
+	public static void main(String[] args) {
+
+		Add nanna = new Add();
+		System.out.println("The result is " + nanna.addNow(12, 13));
+		System.out.println("The result is " + nanna.sub(45, 19));
+
+	}
+
+}
