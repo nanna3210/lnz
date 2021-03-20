929f4fdd1de20b838d394fee3ccffe059ce2cd63
nannacode <nannacode@gmail.com>
2021-03-20 17:34:55 +0530

    Person Class Added ....

---
+++ justEx/src/justEx/Person.java
@@ -0,0 +1,17 @@
+package justEx;
+
+public class Person {
+
+	
+	private String firtName;
+	private String lastName;
+	public String getFirtName() {
+		return firtName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
+}
